f879379a9cb1da05de0086306b68d3854a294d61
tmosest <tmosest@gmail.com>
2018-11-25 16:45:54 -0500

    basic calculator with bugs

--- app/src/main/java/com/tmosest/calculator/MainActivity.kt
+++ app/src/main/java/com/tmosest/calculator/MainActivity.kt
@@ -2,11 +2,102 @@ package com.tmosest.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val result: EditText by lazy(LazyThreadSafetyMode.NONE) { findViewById<EditText>(R.id.result) }
+    private val newNumber by lazy(LazyThreadSafetyMode.NONE) { findViewById<EditText>(R.id.newNumber) }
+    private val displayOperator by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
+
+    // Variables to hold operands and type of calculations
+    private var operand1: Double? = null
+    private var operand2: Double = 0.0
+    private var pendingOperation = "="
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button0: Button = findViewById(R.id.button0)
+        val button1: Button = findViewById(R.id.button1)
+        val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
+        val button4: Button = findViewById(R.id.button4)
+        val button5: Button = findViewById(R.id.button5)
+        val button6: Button = findViewById(R.id.button6)
+        val button7: Button = findViewById(R.id.button7)
+        val button8: Button = findViewById(R.id.button8)
+        val button9: Button = findViewById(R.id.button9)
+        val buttonDot: Button = findViewById(R.id.buttonDot)
+
+        // operations buttons
+        val buttonEquals: Button = findViewById(R.id.buttonEquals)
+        val buttonDivide: Button = findViewById(R.id.buttonDivide)
+        val buttonMultiply: Button = findViewById(R.id.buttonMultiply)
+        val buttonMinus: Button = findViewById(R.id.buttonMinus)
+        val buttonPlus: Button = findViewById(R.id.buttonPlus)
+
+        val listener = View.OnClickListener { v ->
+            val b  = v as Button
+            newNumber.append(b.text)
+        }
+
+        button0.setOnClickListener(listener)
+        button1.setOnClickListener(listener)
+        button2.setOnClickListener(listener)
+        button3.setOnClickListener(listener)
+        button4.setOnClickListener(listener)
+        button5.setOnClickListener(listener)
+        button6.setOnClickListener(listener)
+        button7.setOnClickListener(listener)
+        button8.setOnClickListener(listener)
+        button9.setOnClickListener(listener)
+        buttonDot.setOnClickListener(listener)
+
+        val operationListener = View.OnClickListener { v ->
+            val op = (v as Button).text.toString()
+            val value = newNumber.text.toString()
+            if (value.isNotEmpty()) {
+                performOpertaion(value, op)
+            }
+            pendingOperation = op
+            displayOperator.text = pendingOperation
+        }
+
+        buttonEquals.setOnClickListener(operationListener)
+        buttonMinus.setOnClickListener(operationListener)
+        buttonPlus.setOnClickListener(operationListener)
+        buttonDivide.setOnClickListener(operationListener)
+        buttonMultiply.setOnClickListener(operationListener)
+    }
+
+    private fun performOpertaion(value: String, operation: String) {
+        if (operand1 == null) {
+            operand1 = value.toDouble()
+        } else {
+            operand2 = value.toDouble()
+
+            if (pendingOperation == "=") {
+                pendingOperation = operation
+            }
+
+            when (pendingOperation) {
+                "=" -> operand1 = operand2
+                "/" ->  if (operand2 == 0.0) {
+                            operand1 = Double.NaN
+                        } else {
+                            operand1 = operand1!! / operand2
+                        }
+                "*" -> operand1 = operand1!! * operand2
+                "-" -> operand1 = operand1!! - operand2
+                "+" -> operand1 = operand1!! + operand2
+            }
+
+            result.setText(operand1.toString())
+            newNumber.setText("")
+        }
     }
 }
